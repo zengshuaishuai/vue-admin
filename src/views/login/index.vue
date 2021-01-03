7315e44792bd647a7289bceda2be9fd61536a7cc
zengshuaishuai <17621850377@163.com>
2021-01-03 18:32:57 +0800

    登录menutab实现

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,21 +1,74 @@
 <template>
   <div id="login">
-    This is an login page
-    <el-button>默认按钮</el-button>
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li :class="{'current': item.current}" v-for="(item,index) in menuTab" :key="item.id"  @click="toggleMenu(item)">{{item.txt}}</li>
+      </ul>
+    </div>
   </div>
 
 </template>
 
 <script>
   export default {
-    name: 'login'
+    name: 'login',
+    data(){
+      return {
+        menuTab:[
+          {txt:'登录',current:true},
+          {txt:'注册',current:false}
+        ],
+        isActive: false
+      }
+    },
+    created() {
+    },
+    mounted() {
+      //挂载完成后自动执行
+    },
+    methods:{
+      //写函数的地方
+       toggleMenu(data){
+         this.menuTab.forEach(elem=>{
+           elem.current=false
+         })
+         data.current=true
+       }
+    },
+    props:{},
+    watch:{}
   }
 </script>
 
 <style lang="scss" scoped >
+  body
+  {
+    margin: 0;
+    padding: 0;
+  }
    #login{
      height: 100vh;
-     outline:none;
-     /*background-color: #344a5f;*/
+     /*outline:none;*/
+     background-color: #344a5f;
    }
+  .login-wrap{
+    width: 330px;
+    margin: auto;
+  }
+  .menu-tab{
+    text-align: center;
+    li{
+      display: inline-block;
+      width: 88px;
+      line-height: 36px;
+      font-size: 14px;
+      color: #fff;
+      border-radius: 2px;
+      cursor: pointer;
+    }
+    .current{
+      background-color: rgba(0,0,0,.1);
+    }
+
+  }
 </style>
